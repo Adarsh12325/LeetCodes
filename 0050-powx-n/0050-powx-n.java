69e8e7f90e3750dd8298b0c76a8a8321b6ee3a62
Adarsh Sah <155521173+Adarsh12325@users.noreply.github.com>
2025-12-11 23:22:50 +0530

    Time: 0 ms (100%), Space: 48.2 MB (17.71%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,23 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n==0) return 1.0;
+        if(x==0) return 0.0;
+        if(x==1) return 1.0;
+        long bin = n;
+        double ans = 1;
+        if(n<0){
+            x = 1/x;
+            bin = -bin;
+        }
+        if(x==-1 && n%2==0) return 1.0;
+        else if(x==-1 && n%2!=0) return -1.0;
+        while(bin>0){ 
+            if(bin%2==1){
+                ans *= x;
+            }
+            x*=x;
+            bin/=2;
+        }
+        return ans;
+    }
+}
