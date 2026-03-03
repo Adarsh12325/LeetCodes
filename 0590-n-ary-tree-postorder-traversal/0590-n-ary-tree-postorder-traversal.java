c2fb82522eae9de9b84daf62f74a007f2e79af24
Adarsh Sah <23P31A4402@acet.ac.in>
2026-03-03 13:17:30 +0530

    Time: 0 ms (100%), Space: 46.5 MB (51.47%) - LeetHub

---
+++ 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.java
@@ -0,0 +1,33 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+*/
+
+class Solution {
+    public void norder(Node root, List<Integer> l){
+        if(root == null) return;
+        for(Node child: root.children){
+           norder(child,l); 
+        }
+        l.add(root.val); 
+    }
+    public List<Integer> postorder(Node root) {
+        List<Integer> l = new ArrayList<>();
+        norder(root,l);
+        return l;
+    }
+}
